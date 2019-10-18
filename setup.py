6075a61c276a09cbf510550b7a919b0a5a3c0ccd
Rafael Castillo <rafael@trfs.me>
2019-10-18 00:36:40 -0700

    Fix setuptools install

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ledgeroni',
     version='0.1',
-    py_modules=['ledgeroni'],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'Click',
+        'arrow',
+        'six',
+        'colorama'
     ],
     entry_points='''
         [console_scripts]
